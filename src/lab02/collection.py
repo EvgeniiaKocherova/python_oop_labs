6a5d89f60309c61475779665f452f91b74f86d99
evgeniiakocherova <m2504998@edu.misis.ru>
2026-04-07 14:14:49 +0300

    ..

--- src/lab02/collection.py
+++ src/lab02/collection.py
@@ -1,12 +1,15 @@
 from model import Book
 class Library:
+    """Коллекция книг"""
     def __init__(self):
         self._items = []
 
     def __str__(self):
-        return f"Библиотека, {len(self)} книг"
+       """Красивый вывод библиотеки"""
+       return f"Библиотека, {len(self)} книг"
 
     def add(self, book):
+        """Добавление книги в коллекцию"""
         if not isinstance(book, Book):
             raise ValueError("Можно добавлять только объекты Book")
         for old_book in self._items:
@@ -15,26 +18,32 @@ class Library:
         self._items.append(book)
 
     def remove(self, book):
+        """Удаление книги из коллекции"""
         if not isinstance(book, Book):
             raise ValueError("Можно удалять только объекты Book")
         self._items.remove(book)
 
     def get_all(self):
+        """Вывод содержимого библиотеки"""
         return self._items
     
     def __len__(self):
+        """Количество книг в библиотеке"""
         return len(self._items)
     
     def __iter__(self):
+        """Итерация по библиотеке"""
         return iter(self._items)
     
     def find_by_title(self, title):
+        """Нахождение книги в библиотеке по названию"""
         for book in self._items:
             if book.title == title:
                 return book
         return None
     
     def find_by_author(self, author):
+        """Нахождение всех книг автора в библиотеке"""
         list_of_books = []
         for book in self._items:
             if book.author == author:
